d52cc221167e39845b1ef05897d7aea77596f0b1
Alex Casado <93490385+alexcasado100@users.noreply.github.com>
2022-05-12 17:58:42 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'alexcasado100',
+  youtube: 'channel/UCk6g4N9dhY5It-PmGU7fOBQ',
+  facebook: 'alexcasado100',
+  instagram: 'alexcasado86',
+  twitter: 'alex_casado'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
